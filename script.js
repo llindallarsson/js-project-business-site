56ae1c853afc3e083e9ed9a22b5dd53e7035d4ee
llindallarsson <larsson.ll@hotmail.com>
2025-02-20 21:24:29 +0100

    added opening hours to the page

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ hamburgerMenu.addEventListener("click", () => {
   }
 
 });
+
+const dayOfWeek = (new Date).getDay();
+const openHours = ["Open today 10.00–15.00", //sunday
+  "Closed today", // monday
+  "Open today 10.00–18.00", // tuesday
+  "Open today 10.00–18.00", // wendsday 
+  "Closed today", // thursday
+  "Open today 10.00–20.00", // friday
+  "Open today 10.00–15.00"] // saturday 
+
+const todayHours = openHours[dayOfWeek];
+document.getElementById("openHours").innerHTML = todayHours;
